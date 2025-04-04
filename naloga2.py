7967bb412a4945921534d41b5562a008cb07a6fa
Ardi <ardi.bytyqi@student.um.si>
2025-04-04 20:04:03 +0200

    Implemented filtriraj_z_gaussovim_jedrom function and used it in main

--- naloga2.py
+++ naloga2.py
@@ -15,7 +15,7 @@ def konvolucija(slika, jedro):
     # Inicializacija izhodne slike
     rezultat = np.zeros_like(slika, dtype=np.float32)
 
-    # Izvedba konvolucije 
+    # Izvedba konvolucije
     for i in range(visina):
         for j in range(sirina):
             vrednost = 0
@@ -35,7 +35,26 @@ def konvolucija(slika, jedro):
 
 def filtriraj_z_gaussovim_jedrom(slika, sigma):
     '''Filtrira sliko z Gaussovim jedrom.'''
-    pass
+    # Izračun velikosti jedra po formuli
+    velikost_jedra = int((2 * sigma) * 2 + 1)
+    
+    # Ustvarimo prazno jedro
+    jedro = np.zeros((velikost_jedra, velikost_jedra), dtype=np.float32)
+    
+    # Izračunamo vrednost k po formuli
+    k = velikost_jedra / 2 - 1/2
+    
+    # Izračunamo vrednosti znotraj jedra po formuli
+    for i in range(velikost_jedra):
+        for j in range(velikost_jedra):
+            x = i - k
+            y = j - k
+            jedro[i, j] = (1 / (2 * np.pi * sigma**2)) * np.exp(-((i-k-1)**2 + (j-k-1)**2) / (2 * sigma**2))
+
+    
+    # Uporabimo prej implementirano funkcijo konvolucija za filtriranje slike
+    return konvolucija(slika, jedro)
+
 
 
 def filtriraj_sobel_smer(slika):
@@ -43,5 +62,35 @@ def filtriraj_sobel_smer(slika):
     pass
 
 if __name__ == '__main__':
-    pass
+    # Nalozim lenna sliko
+    slika = cv.imread(".utils/lenna.png", cv.IMREAD_GRAYSCALE)
+    if slika is None:
+        print("Napaka pri nalaganju slike!")
+        exit()
+    
+    # Pretvorimo sliko v float32 za natančnejše računanje
+    slika = np.float32(slika)
+    
+    # Testirajmo funkcijo filtriranje z gaussovim jedrom z razlicnimi sigmami
+    sigma_vrednosti = [0.5, 1.0, 2.0, 10.0]
+    
+    # Prvo pokaze originalno sliko
+    cv.imshow("Originalna slika", np.uint8(slika))
+    
+    # Filtriramo sliko z različnimi vrednostmi sigma
+    for sigma in sigma_vrednosti:
+        filtrirana_slika = filtriraj_z_gaussovim_jedrom(slika, sigma)
+        
+        # Pretvorimo rezultat nazaj v uint8 za prikaz
+        filtrirana_slika_uint8 = np.uint8(np.clip(filtrirana_slika, 0, 255))
+        
+        # Prikažemo filtrirano sliko
+        cv.imshow(f"Gaussov filter (sigma={sigma})", filtrirana_slika_uint8)
+        
+        # Izpišemo velikost uporabljenega jedra
+        velikost_jedra = int((2 * sigma) * 2 + 1)
+        print(f"Sigma: {sigma}, Velikost jedra: {velikost_jedra}x{velikost_jedra}")
+    
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
