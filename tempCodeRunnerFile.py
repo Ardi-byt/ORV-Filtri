3de611b6dd758af14dbeb0b15232e45429b47f2c
Ardi <ardi.bytyqi@student.um.si>
2025-04-06 01:49:44 +0200

    Removed unnecessary file

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-slika_barvna_orig = cv.cvtColor(np.uint8(slika), cv.COLOR_GRAY2BGR)
-    slika_barvna_temna = cv.cvtColor(np.uint8(slika_temna), cv.COLOR_GRAY2BGR)
-    slika_barvna_svetla = cv.cvtColor(np.uint8(slika_svetla), cv.COLOR_GRAY2BGR)
